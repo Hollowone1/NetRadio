fae8d771a75fee4c4e0b6d471d9221a2724ee8c9
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-11-07 16:44:43 +0100

    initialisation du projet

--- server/config/routes.php
+++ server/config/routes.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 use Slim\App;
 
 return function (App $app) {
-    $app->get("/", function () {
-        echo "hello word";
+    $app->get("/", function ($req, $res, $args) {
+        $res->getBody()->write("hello word !");
+        return $res;
     });
 };
