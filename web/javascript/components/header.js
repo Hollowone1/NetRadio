dccb628cd42d24aa9cd3294ca87fc1733d0e4e97
Antoine Stoecklin <astoeck@hotmail.fr>
2023-12-19 15:05:23 +0100

    main components done

---
+++ web/javascript/components/header.js
@@ -0,0 +1,17 @@
+Vue.component('navbar', {
+    data: function () {
+      return {
+        
+      }
+    },
+    template:` <div class="header">
+    <img class="header-logo" src="..." alt="Logo NET RADIO">
+    <div class="header-boutons">
+        <input class="search" type="search" src="../assets/search.svg" placeholder="Search for...">
+        <!--si on est  connecté-->
+        <embed class="header-user-co" src="../assets/user.svg"/>
+        <!--si on n'est pas connecté-->
+        <button class="header-boutons-connecter">Se connecter</button>
+    </div>
+</div>`
+})
