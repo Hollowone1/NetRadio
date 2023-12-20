0402ffb7a56e675bbbc62f15b09bd081e82b5606
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-12-20 12:37:37 +0100

    les DTO de l'application avec chacun la fonction toArray

---
+++ server/src/domaine/dto/CreneauDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace radio\net\domaine\dto;
+
+class CreneauDTO
+{
+    public int $id;
+    public int $joutSemaine;
+    public string $heureDeDepart;
+    public string $heureDeFin;
+    public EmissionDTO $emissionDTO;
+
+    public function __construct($p_id, $p_jourSemaine, $p_heureDepart, $p_heureFin, $p_emission) {
+        $this->id = $p_id;
+        $this->joutSemaine = $p_jourSemaine;
+        $this->heureDeDepart = $p_heureDepart;
+        $this->heureDeFin = $p_heureFin;
+        $this->emissionDTO = $p_emission;
+    }
+
+    public function toArray () {
+        return [
+            'id' => $this->id,
+            'jourSemaine' => $this->joutSemaine,
+            'heureDepart' => $this->heureDeDepart,
+            'heureFin' => $this->heureDeFin,
+            'emission' => $this->emissionDTO
+        ];
+    }
+}
--- server/src/domaine/dto/EmissionDTO.php
+++ server/src/domaine/dto/EmissionDTO.php
@@ -2,17 +2,34 @@
 
 namespace radio\net\domaine\dto;
 
+use radio\net\domaine\entities\User;
+
 class EmissionDTO extends DTO
 {
-    public $id;
+    public int $id;
+    public string $titre;
+    public string $description;
+    public string $theme;
+    public string $photo;
+    public User $user;
 
-    public function __construct () {
-        $this->id = 1;
+    public function __construct ($p_id, $p_titre, $p_description, $p_theme, $p_photo, $p_user) {
+        $this->id = $p_id;
+        $this->titre = $p_titre;
+        $this->description = $p_description;
+        $this->theme = $p_theme;
+        $this->photo = $p_photo;
+        $this->user = $p_user;
     }
 
     public function toArray () {
         return [
-            'id' => $this->id
+            'id' => $this->id,
+            'titre' => $this->titre,
+            'description'=> $this->description,
+            'theme' => $this->theme,
+            'photo' => $this->photo,
+            'user' => $this->user
         ];
     }
 }
