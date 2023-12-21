cd2b6264cb037868495c85db71a54245492ca4a0
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-12-21 14:46:33 +0100

    config

--- server/config/routes.php
+++ server/config/routes.php
@@ -1,11 +1,23 @@
 <?php
 declare(strict_types=1);
 
+use radio\net\app\action\podcast\GetAllPodcasts;
+use radio\net\app\action\podcast\GetPodcastByDate;
+use radio\net\app\action\podcast\GetPodcastByEmission;
+use radio\net\app\action\podcast\GetPodcastByIdAction;
 use Slim\App;
 
 return function (App $app) {
 
-    $app->get("/podcast/{id_podcast}[/]", \radio\net\app\action\GetPodcastByIdAction::class);
-    $app->get("/podcasts", \radio\net\app\action\GetAllPodcasts::class);
-    $app->get("/podcasts/date", \radio\net\app\action\GetPodcastByDate::class);
+    //podcast
+    $app->get("/podcast/{id_podcast}[/]", GetPodcastByIdAction::class);
+    $app->get("/podcasts[/]", GetAllPodcasts::class);
+    $app->get("/podcasts/date", GetPodcastByDate::class);
+
+    //emission
+    $app->get("/emission/{id_emission}/podcasts", GetPodcastByEmission::class);
+    $app->get("/emission/{id_emission}[/]", \radio\net\app\action\emission\GetEmissionById::class);
+
+    //users
+    $app->get("/users", \radio\net\app\action\user\GetUserAllInfo::class);
 };
--- server/config/services_dependencies.php
+++ server/config/services_dependencies.php
@@ -1,10 +1,17 @@
 <?php
 
 use Psr\Container\ContainerInterface;
+use radio\net\domaine\service\emission\EmissionService;
 use radio\net\domaine\service\podcast\PodcastService;
 
 return [
     'PodcastService' => function (ContainerInterface $container) {
         return new PodcastService();
+    },
+    'EmissionService' => function (ContainerInterface $container) {
+        return new EmissionService();
+    },
+    'UserService' => function (ContainerInterface $container) {
+        return new \radio\net\domaine\service\user\UserService();
     }
 ];
