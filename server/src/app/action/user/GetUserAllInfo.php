409690e5f7a3a960bb2634ec272c1b3cd463c41f
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-12-21 14:45:42 +0100

    action et service user

---
+++ server/src/app/action/user/GetUserAllInfo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace radio\net\app\action\user;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use radio\net\domaine\service\user\iUserService;
+use radio\net\domaine\service\user\UserNotFoundException;
+use Slim\Exception\HttpNotFoundException;
+
+class GetUserAllInfo extends \radio\net\app\action\Action
+{
+
+    private iUserService $service;
+
+    public function __construct ($userService) {
+        $this->service = $userService;
+    }
+    function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    {
+        try {
+            $user = $this->service->GetAllInfoUser();
+            $data = [
+                'type' => 'resource',
+                'users' => $user
+            ];
+            $response->getBody()->write(json_encode($data));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+        } catch (UserNotFoundException $e) {
+            throw new HttpNotFoundException($request ,$e->getMessage());
+        }
+    }
+}
--- server/src/domaine/service/user/UserService.php
+++ server/src/domaine/service/user/UserService.php
@@ -6,6 +6,20 @@ use radio\net\domaine\entities\User;
 
 class UserService implements iUserService
 {
+
+    public function GetAllInfoUser () {
+        try {
+            $users = User::all();
+            $usersDTO = [];
+            foreach ($users as $user) {
+                $usersDTO [] = $user->toDTO();
+            }
+            return $usersDTO;
+        } catch (UserNotFoundException $e) {
+            throw new UserNotFoundException("User not found");
+        }
+    }
+
     public function GetInfoUserById ($id) {
         try {
             $user = User::find($id);
