245fd02a9e248cc9aa61f7d1b237c4b983bab80e
Antoine Stoecklin <astoeck@hotmail.fr>
2023-12-21 14:45:05 +0100

    views done

---
+++ web/app/pages/liste-podcast.vue
@@ -0,0 +1,76 @@
+<template>
+    <header>
+    </header>
+    <navbar>
+
+    </navbar>
+    <main>
+        <section>
+            <i></i>
+            <h2>Nom Emission</h2>
+            <span>En direct</span>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Nostrum minima in rem natus non expedita, 
+                saepe commodi neque facere molestiae ipsam harum. 
+                Inventore voluptatum praesentium adipisci numquam deleniti totam sapiente!
+            </p>
+            <button><i></i>Ecoutez</button>
+            <img src="../assets/Desktop.png" alt="emission en direct">
+        </section>
+        <section>
+            <input type="text" placeholder="Rechercher">
+            <div class="programmes">
+                <h3>Aujourd'hui</h3>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <h3>Cette semaine</h3>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <h3>Ce mois ci</h3>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+                <div class="podcast">
+                    <h4>Le titre du podcast</h4>
+                    <span>Nom d'émission</span>
+                </div>
+            </div>
+        </section>
+        <a href="">Découvrir d'autres podcasts</a>
+    </main>
+
+    <footer>
+        
+    </footer>
+</template>
+
+<script setup>
+    import navbar from '../components/nav'
+    import header from '../components/header'
+    import footer from '../components/footer'
+</script>
