537f060ac683a55189369bdfa661315f06da1f65
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-12-21 14:44:30 +0100

    les emissions avec le services, l'entites et les actions

--- server/src/app/action/emission/CreerEmission.php
+++ server/src/app/action/emission/CreerEmission.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace radio\net\app\action;
+namespace radio\net\app\action\emission;
 
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use radio\net\app\action\Action;
 
 class CreerEmission extends Action
 {
