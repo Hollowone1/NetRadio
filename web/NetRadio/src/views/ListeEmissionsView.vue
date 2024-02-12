2b0a3ae6f003ac0d6d2f34ae89df6949da000d16
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-02-12 14:18:54 +0100

    liste émissions

--- web/NetRadio/src/views/ListeEmissionsView.vue
+++ web/NetRadio/src/views/ListeEmissionsView.vue
@@ -1,57 +1,60 @@
 <script>
 import EnDirect from '@/components/EnDirect.vue'
 import HeaderComponent from "@/components/HeaderComponent.vue";
+
 export default {
   components: {
     EnDirect,
     HeaderComponent
   },
   data() {
-    return {
-
-    }
+    return {}
   }
 }
 </script>
 <template>
-<main>
+  <main>
     <en-direct></en-direct>
 
-        <div class="emissions">
-            <h2>Toutes les émissions</h2>
-            <div class="theme">
-                <h3>Thématique 1</h3>
-                <div class="emissions-liste">
-                    <section class="emission">
-                        <p>Le titre de l'émission</p>
-                        <p>PRÉSENTATEUR</p>
-                    </section>
-                    <section class="emission">
-                        <p>Le titre de l'émission</p>
-                        <p>PRÉSENTATEUR</p>
-                    </section>
-                    <section class="emission">
-                        <p>Le titre de l'émission</p>
-                        <p>PRÉSENTATEUR</p>
-                    </section>
-                    <section class="emission">
-                        <p>Le titre de l'émission</p>
-                        <p>PRÉSENTATEUR</p>
-                    </section>
-                    <section class="emission">
-                        <p>Le titre de l'émission</p>
-                        <p>PRÉSENTATEUR</p>
-                    </section>
-                    <section class="emission">
-                        <p>Le titre de l'émission</p>
-                        <p>PRÉSENTATEUR</p>
-                    </section>
-                </div>
-            </div>
+    <div class="emissions">
+      <h2>Toutes les émissions</h2>
+      <div class="theme">
+        <h3>Thématique 1</h3>
+        <div class="emissions-liste">
+          <section class="emission">
+            <p>Le titre de l'émission</p>
+            <p>PRÉSENTATEUR</p>
+          </section>
+          <section class="emission">
+            <p>Le titre de l'émission</p>
+            <p>PRÉSENTATEUR</p>
+          </section>
+          <section class="emission">
+            <p>Le titre de l'émission</p>
+            <p>PRÉSENTATEUR</p>
+          </section>
+          <section class="emission">
+            <p>Le titre de l'émission</p>
+            <p>PRÉSENTATEUR</p>
+          </section>
+          <section class="emission">
+            <p>Le titre de l'émission</p>
+            <p>PRÉSENTATEUR</p>
+          </section>
+          <section class="emission">
+            <p>Le titre de l'émission</p>
+            <p>PRÉSENTATEUR</p>
+          </section>
         </div>
-    </main>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped lang="scss">
-
+.emissions {
+  padding-top: 1em;
+  padding-left: 3em;
+  padding-right: 3em;
+}
 </style>
