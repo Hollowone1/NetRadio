709f0b4dbf857377d6f6dc5f01078d6a07b8852a
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-12 10:39:38 +0100

    views+ router

--- web/NetRadio/src/router/index.js
+++ web/NetRadio/src/router/index.js
@@ -1,22 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AccueilView from '../views/HomeView.vue'
+import GrilleDesProgrammesViewVue from '@/views/Grille-des-programmesView.vue'
+import ListeEmissionViewVue from '@/views/Liste-emission-view.vue'
+import ListePodcastsViewVue from '@/views/Liste-podcastsView.vue'
+import PresentationEmissionViewVue from '@/views/Presentation-emissionView.vue'
+import ResultatEmissionViewVue from '@/views/Resultat-emissionView.vue'
+import ResultatPodcastViewVue from '@/views/Resultat-podcastView.vue'
+import ToutesLesEmissionsViewVue from '@/views/Toutes-les-emissionsView.vue'
+import ProfilUtilisateurViewVue from '@/views/Profil-utilisateurView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'accueil',
+      component: AccueilView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/grille-des-programmes',
+      name: 'grille des programmes',
+      component: GrilleDesProgrammesViewVue
+    },
+    {
+      path: '/liste-des-emissions',
+      name: 'liste des emissions',
+      component: ListeEmissionViewVue
+    },
+    {
+      path: '/liste-des-podcasts',
+      name: 'liste des podcasts',
+      component: ListePodcastsViewVue
+    },
+    {
+      path: '/presentation-emission',
+      name: 'presentation des émissions',
+      component: PresentationEmissionViewVue
+    },
+    {
+      path: '/resultat-emission',
+      name: 'résultat des émissions',
+      component: ResultatEmissionViewVue
+    },
+    {
+      path: '/resultat-podcast',
+      name: 'résultat des podcast',
+      component: ResultatPodcastViewVue
+    },
+    {
+      path: '/toutes-les-emissions',
+      name: 'résultat des emissions',
+      component: ToutesLesEmissionsViewVue
+    },
+    {
+      path: '/profil',
+      name: 'profil-utilisateurs',
+      component: ProfilUtilisateurViewVue
+    },
+
   ]
 })
 
