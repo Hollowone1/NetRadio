783d971d0b072f6634de06c0f4500fe16a2f02ee
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-12 11:01:25 +0100

    puted routerview

---
+++ web/NetRadio/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+}
+</script>
+
+<style>
+@import './assets/main.css';
+</style>
