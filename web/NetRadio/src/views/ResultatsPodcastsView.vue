53b1d4dea17d9195500773b4dc71d18aef6f5ae7
tamourgh1u <grindmoney19@gmail.com>
2024-02-12 14:58:27 +0100

    resultats emissions / podcasts

--- web/NetRadio/src/views/ResultatsPodcastsView.vue
+++ web/NetRadio/src/views/ResultatsPodcastsView.vue
@@ -1,21 +1,22 @@
 <script>
+import {RouterView } from 'vue-router'
+import EnDirect from '@/components/EnDirect.vue';
 
+export default {
+  components: {
+    RouterView,
+    EnDirect
+  },
+  data() {
+    return {}
+  }
+}
 </script>
 
 <template>
     <main>
-        <section class="direct">
-            <div class="direct-infos">
-                <div class="direct-infos-titre">
-                    <embed src="../assets/direct.svg"/>
-                    <h1>Nom émission</h1>
-                </div>
-                <p class="direct-infos-sous-titre">EN DIRECT</p>
-                <p class="direct-infos-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button class="direct-infos-ecouter">Écouter</button>
-            </div>
-            <img class="direct-image" src="../assets/Desktop.png" alt="image de l'émission en direct">
-        </section>
+        <en-direct></en-direct>
+        <RouterView />
 
         <div class="resultats">
             <h2>Résultats de votre recherche</h2>
