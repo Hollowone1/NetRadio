e14ddefbf6995ea383953770278d643292fc174f
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-12 11:18:50 +0100

    toutes les emissions

--- web/NetRadio/src/views/ToutesEmissionsView.vue
+++ web/NetRadio/src/views/ToutesEmissionsView.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-
+    <main>
+        <h2>Liste des émissions</h2>
+        <div class="utilisateurs">
+            <div class="role">
+                <span>titre de l'émission</span>
+                <img src="" alt="edit">
+            </div>
+            <div class="info">
+                <p>Présentateur</p>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
