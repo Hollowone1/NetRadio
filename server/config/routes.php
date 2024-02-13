99d3b7e77ee3654348a0297c97fedb12da4fdd94
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-02-13 11:25:43 +0100

    Ajout d'une route et d'une action pour récupérer un créneau par son id

--- server/config/routes.php
+++ server/config/routes.php
@@ -2,11 +2,22 @@
 declare(strict_types=1);
 
 use radio\net\app\action\creneau\GetAllCreneaux;
+use radio\net\app\action\creneau\GetCreneauById;
+use radio\net\app\action\emission\GetEmissionById;
+use radio\net\app\action\emission\GetEmissionByTheme;
+use radio\net\app\action\emission\GetEmissionsAction;
+use radio\net\app\action\playlist\GetPlaylistByEmailUserAction;
+use radio\net\app\action\playlist\GetPlaylistByIdAction;
+use radio\net\app\action\playlist\PostPlaylist;
 use radio\net\app\action\podcast\GetAllPodcasts;
-use radio\net\app\action\podcast\GetPodcastByDate;
 use radio\net\app\action\podcast\GetPodcastByEmission;
 use radio\net\app\action\podcast\GetPodcastByIdAction;
+use radio\net\app\action\podcast\PostPodcast;
 use radio\net\app\action\son\GetAllSons;
+use radio\net\app\action\son\GetSonByIdAction;
+use radio\net\app\action\son\GetSonsByPlaylistId;
+use radio\net\app\action\user\GetUserAllInfo;
+use radio\net\app\action\user\GetUserByMail;
 use Slim\App;
 
 return function (App $app) {
@@ -15,40 +26,41 @@ return function (App $app) {
     $app->group('/podcasts', function ($app) {
         $app->get("/{id_podcast}[/]", GetPodcastByIdAction::class)->setName('podcast.show');
         $app->get("[/]", GetAllPodcasts::class)->setName('podcast.index');
-        $app->post("[/]", \radio\net\app\action\podcast\PostPodcast::class)->setName('');
+        $app->post("[/]", PostPodcast::class)->setName('');
     });
 
     //emission
     $app->group('/emissions', function ($app) {
         $app->get("/{id_emission}/podcasts", GetPodcastByEmission::class)->setName('');
-        $app->get("/{id_emission}[/]", \radio\net\app\action\emission\GetEmissionById::class)->setName('emission.show');
-        $app->get("[/]", \radio\net\app\action\emission\GetEmissionsAction::class)->setName('/emissions')->setName('emission.index');
-        $app->get("/theme/{theme}", \radio\net\app\action\emission\GetEmissionByTheme::class)->setName('/emissions/theme');
+        $app->get("/{id_emission}[/]", GetEmissionById::class)->setName('emission.show');
+        $app->get("[/]", GetEmissionsAction::class)->setName('/emissions')->setName('emission.index');
+        $app->get("/theme/{theme}", GetEmissionByTheme::class)->setName('/emissions/theme');
     });
 
     //users
     $app->group('/users', function ($app) {
-        $app->get("[/]", \radio\net\app\action\user\GetUserAllInfo::class);
-        $app->get("/{id_user}[/]", \radio\net\app\action\user\GetUserByMail::class)->setName('/user/{id_user}[/]');
-        $app->get("/{email_user}/playlists", \radio\net\app\action\playlist\GetPlaylistByEmailUserAction::class)->setName('/user/{email_user}/playlist');
+        $app->get("[/]", GetUserAllInfo::class);
+        $app->get("/{id_user}[/]", GetUserByMail::class)->setName('/user/{id_user}[/]');
+        $app->get("/{email_user}/playlists", GetPlaylistByEmailUserAction::class)->setName('/user/{email_user}/playlist');
 
     });
 
     //sons
     $app->group('/sons', function ($app) {
         $app->get("[/]", GetAllSons::class);
-        $app->get("/{id_son}[/]", \radio\net\app\action\son\GetSonByIdAction::class)->setName('/son/{id_son}[/]');
-        $app->get("/playlist/{id_playlist}[/]", \radio\net\app\action\son\GetSonsByPlaylistId::class)->setName('/son/playlist/{id_playlist}[/]');
+        $app->get("/{id_son}[/]", GetSonByIdAction::class)->setName('/son/{id_son}[/]');
+        $app->get("/playlist/{id_playlist}[/]", GetSonsByPlaylistId::class)->setName('/son/playlist/{id_playlist}[/]');
     });
 
     //playlist
     $app->group('/playlists', function ($app) {
-        $app->get("/{id_playlist}[/]", \radio\net\app\action\playlist\GetPlaylistByIdAction::class)->setName('/playlist/{id_playlist}[/]');
-        $app->post("[/]", \radio\net\app\action\playlist\PostPlaylist::class)->setName("/playlist");
+        $app->get("/{id_playlist}[/]", GetPlaylistByIdAction::class)->setName('/playlist/{id_playlist}[/]');
+        $app->post("[/]", PostPlaylist::class)->setName("/playlist");
     });
 
     // creneau
     $app->group('/creneaux', function ($app) {
         $app->get("[/]", GetAllCreneaux::class);
+        $app->get("/{id}[/]", GetCreneauById::class);
     });
 };
