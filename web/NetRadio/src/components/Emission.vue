3f515404a08c871696ff59f30bd3e6a0e418443a
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-13 14:04:15 +0100

    push emission boucle et appel api

--- web/NetRadio/src/components/Emission.vue
+++ web/NetRadio/src/components/Emission.vue
@@ -1,42 +1,47 @@
 <template>
-<div class="emissions">
-      <h2>Toutes les émissions</h2>
-      <div class="theme">
-        <h3>Thématique 1</h3>
-        <div class="emissions-liste">
-          <section class="emission" v-for="emission in emissions" :key="emission.id">
-            <p>{{ emission.titre }}</p>
-            <p>{{ emission.theme }}</p>
-            <p>{{ emission.user_mail }}</p>
-          </section>
-        </div>
+  <div class="emissions">
+    <h2>Toutes les émissions</h2>
+    <div class="theme">
+      <h3>Thématique 1</h3>
+      <div class="emissions-liste">
+        <section class="emission" v-for="emission in emissionsData" :key="emission.id">
+          <p>{{ emission.titre }}</p>
+          <p>{{ emission.theme }}</p>
+          <p>{{ emission.user_mail }}</p>
+        </section>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
-  <script>
-  import axios from 'axios';
-  import instance from '@/plugin/api'
+<script>
+import axios from 'axios';
+import instance from '@/plugin/api'
 
-  export default {
-    data(){
-      return{
-        emission: []
-      }
-    },
-    created() {
+export default {
+  data(){
+    return{
+      emissionsData: []
+    }
+  },
+  created() {
     axios.get(`${instance}/emissions`)
-      .then((response) => response.json())
-      .then((data) => {
-        this.emissions = data;
+      .then((response) => {
+        console.log(response)
+        if (response.data) {
+          this.emissionsData = response.data;
+        } else {
+          throw new Error('Emissions data not found');
+        }
       })
       .catch((error) => console.log(error));
   },
-    props: {
-      emission: {
-        type: Object,
-        required: true,
-      },
+  props: {
+    emission: {
+      type: Object,
+      required: true,
+      default: () => ({})
     },
-  };
-  </script>
+  },
+};
+</script>
