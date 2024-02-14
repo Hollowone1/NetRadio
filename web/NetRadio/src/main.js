4fc367f25f942659c1f5e112660d15412ea72f7e
tamourgh1u <grindmoney19@gmail.com>
2024-02-14 15:43:41 +0100

    calendrier (ajt temporairement à l'accueil)

--- web/NetRadio/src/main.js
+++ web/NetRadio/src/main.js
@@ -3,12 +3,15 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import api from '@/plugin/api.js'
+import 'v-calendar/style.css';
+import VCalendar from 'v-calendar';
 
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(VCalendar, {})
 
 app.use(createPinia())
 app.use(router)
