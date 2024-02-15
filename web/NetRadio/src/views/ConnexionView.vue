b618b36ee20d999472755312b9e66657749b0170
tamourgh1u <grindmoney19@gmail.com>
2024-02-15 16:06:08 +0100

    authst

--- web/NetRadio/src/views/ConnexionView.vue
+++ web/NetRadio/src/views/ConnexionView.vue
@@ -41,7 +41,7 @@ export default {
 
 .form-group{
   display:block;
-  text-align: center;
+  padding: 1em;
 }
 
 
@@ -90,6 +90,7 @@ label {
   justify-content: center;
   gap: 0;
   align-items: stretch;
+  text-align: left;
 }
 
 button {
@@ -97,7 +98,7 @@ button {
   font-family: "Inter", Helvetica, Arial, sans-serif;
   font-weight: 400;
   display: flex;
-  width: auto;
+  width: 30%;
   justify-content: center;
   align-items: center;
   flex: 1 0 0;
@@ -105,23 +106,26 @@ button {
   background-color: #A568BB;
   color: #D9D9D9;
   font-size: 1em;
-  padding: 0.5em 1em 0.5em 1em;
+  padding: 1em;
   margin: 0.5em 0em 0.5em 0em;
   border-radius: 0px;
   transition: border-radius 0.3s;
   margin-left: auto;
   margin-right: auto;
+  margin-bottom: 2em;
 }
 button:hover {
   background-color: #8d4ba5;
   border: 2px solid #b291fa;
+  color: white;
 }
 
 label {
   font-size: inherit;
   color: inherit;
   font-weight: inherit;
-  height: 30px;
+  height: 50px;
+  
 }
 
 input {
@@ -131,15 +135,21 @@ input {
   height: 40px;
   background-color: #E9E9E9;
   margin-bottom: 10px;
+  width: 30%;
+  padding: 0.2em;
+
 }
 
 hr {
-  width: 30em;
+  width: 60em;
 }
 .register{
   text-align: center;
 }
-
+input{
+  border: transparent;
+  border-bottom: 1px solid #a2a2a2;
+}
 a{
   text-decoration: none;
   color: #b291fa;
@@ -147,6 +157,9 @@ a{
 form {
   margin-top: 40px;
   margin-bottom: 40px;
+  margin: 5em;
+  text-align: center;
+
 }
 
 </style>
