9a904c90f2e60e450bb4633df49b16945a30fb08
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-02-15 12:43:18 +0100

    émissions change ds le flex

--- web/NetRadio/src/components/Emission.vue
+++ web/NetRadio/src/components/Emission.vue
@@ -34,6 +34,7 @@ export default {
 <template>
   <section v-image="emission.photo" class="emission">
     <img v-if="edit" @click="$emit('edit')" src="/icons/edit.svg" alt="edit">
+    <div v-if="!edit"></div>
     <div>
       <p>{{ emission.titre }}</p>
       <p>{{ emission.user }}</p>
