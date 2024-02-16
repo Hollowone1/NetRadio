9534425ace2a449578b431d946efffae9f2ee8f0
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-16 12:14:26 +0100

    toutes emissions link api done

--- web/NetRadio/src/views/ToutesEmissionsView.vue
+++ web/NetRadio/src/views/ToutesEmissionsView.vue
@@ -1,20 +1,43 @@
 <script>
+import EnDirect from '@/components/EnDirect.vue'
+import Emission from '@/components/Emission.vue'
 
+export default {
+  components: {
+    EnDirect,
+    Emission
+  },
+  data() {
+    return {
+      emissions: []
+    }
+  },
+  created() {
+    this.$api.get("/emissions")
+        .then((response) => {
+          this.emissions = response.data.emission
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+  }
+}
 </script>
 
 <template>
-    <main>
-        <h2>Liste des émissions</h2>
-        <div class="utilisateurs">
-            <div class="role">
-                <span>titre de l'émission</span>
-                <img src="" alt="edit">
-            </div>
-            <div class="info">
-                <p>Présentateur</p>
-            </div>
-        </div>
-    </main>
+    
+  <main>
+    <h2>Liste des émissions</h2>
+    <div class="utilisateurs">
+      <div class="role" v-for="(emission, index) in emissions" :key="index">
+        <span>{{ emission.titre }}</span>
+        <img src="" alt="edit">
+      </div>
+      <div class="info">
+        <p>{{ emission.user_mail }}</p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped lang="scss">
